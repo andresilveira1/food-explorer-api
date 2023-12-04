ebb1ebb2f3768858c830ec7671c7e3aaa137a242
André Silveira <andre.silveira1silva@gmail.com>
2023-12-04 20:57:55 -0300

    feat: Added session route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,14 @@
 const { Router } = require('express')
 const usersRouter = require('./users.routes')
-const menuRouter = require('./menu.routes')
+const menusRouter = require('./menus.routes')
+const favoritesRouter = require('./favorites.routes')
+const sessionsRouter = require('./sessions.routes')
 
 const routes = Router()
 
+routes.use('/sessions', sessionsRouter)
 routes.use('/users', usersRouter)
-routes.use('/menu', menuRouter)
+routes.use('/menu', menusRouter)
+routes.use('/favorites', favoritesRouter)
 
 module.exports = routes
