f274a01cfe8ee443c4074d86bef26672ba08bbd2
André Silveira Silva <andre.silveira1silva@gmail.com>
2023-12-16 22:10:14 -0300

    update routes name

--- src/routes/menu.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express')
-const MenuController = require('../controllers/MenuController')
-
-const menuRoutes = new Router()
-const menuController = new MenuController()
-
-menuRoutes.post('/', menuController.create)
-
-module.exports = menuRoutes
