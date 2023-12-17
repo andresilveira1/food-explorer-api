c385195ea0ef9e650f24e2f5f35696f605e357d7
André Silveira <andre.silveira1silva@gmail.com>
2023-12-16 22:07:54 -0300

    feat: Routes - Added tag routes.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,17 @@
 const { Router } = require('express')
+
 const usersRouter = require('./users.routes')
 const menusRouter = require('./menus.routes')
 const favoritesRouter = require('./favorites.routes')
 const sessionsRouter = require('./sessions.routes')
+const tagsRouter = require('./tags.routes')
 
 const routes = Router()
 
 routes.use('/sessions', sessionsRouter)
 routes.use('/users', usersRouter)
-routes.use('/menu', menusRouter)
+routes.use('/menus', menusRouter)
 routes.use('/favorites', favoritesRouter)
+routes.use('/tags', tagsRouter)
 
 module.exports = routes
