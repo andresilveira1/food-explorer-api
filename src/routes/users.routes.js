bcba85316555b217c326cf1ea6797da4b81ca8a4
André Silveira <andre.silveira1silva@gmail.com>
2023-12-17 21:58:00 -0300

    feat: fix on router initialization

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,7 +1,7 @@
 const { Router } = require('express')
 const UsersController = require('../controllers/UsersController')
 
-const usersRoutes = new Router()
+const usersRoutes = Router()
 const usersController = new UsersController()
 
 usersRoutes.post('/', usersController.create)
