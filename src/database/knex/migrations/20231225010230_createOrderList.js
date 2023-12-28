9d7e3b501bb972c7a96fe046b18411a68ec28922
André Silveira <andre.silveira1silva@gmail.com>
2023-12-27 23:03:34 -0300

    feat: Update migrations

--- src/database/knex/migrations/20231225010230_createOrderList.js
+++ src/database/knex/migrations/20231225010230_createOrderList.js
@@ -1,10 +1,7 @@
 exports.up = (knex) =>
   knex.schema.createTable('order_list', (table) => {
     table.increments('id')
-    table.text('name')
     table.integer('quantity')
-    table.integer('price')
-    table.text('image')
     table.integer('menu_id').references('id').inTable('menus')
     table
       .integer('user_id')
