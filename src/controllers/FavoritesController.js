4a182b9405cd773fec7002345d7f1add4013264c
André Silveira <andre.silveira1silva@gmail.com>
2023-12-27 23:04:30 -0300

    feat: Update favorites controller

--- src/controllers/FavoritesController.js
+++ src/controllers/FavoritesController.js
@@ -2,12 +2,10 @@ const knex = require('../database/knex')
 
 class FavoritesController {
   async create(req, res) {
-    const { name, menu_id, image } = req.body
+    const { menu_id } = req.body
     const user_id = req.user.id
 
     await knex('favorites').insert({
-      name,
-      image,
       user_id,
       menu_id,
     })
@@ -18,7 +16,16 @@ class FavoritesController {
   async index(req, res) {
     const { user_id } = req.params
 
-    const fav = await knex('favorites').where({ user_id }).groupBy('name')
+    const fav = await knex('menus')
+      .select([
+        'menus.name',
+        'menus.image',
+        'favorites.id',
+        'favorites.menu_id',
+      ])
+      .where('favorites.user_id', '=', user_id)
+      .innerJoin('favorites', 'favorites.menu_id', 'menus.id')
+      .groupBy('menus.name')
 
     return res.json(fav)
   }
