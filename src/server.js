2434ccb86ca305bed4224f7b63c79336d873bfa2
André Silveira <andre.silveira1silva@gmail.com>
2024-03-01 19:21:39 -0300

    fix: access control middleware

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ app.use(
 app.use((req, res, next) => {
   res.setHeader(
     'Access-Control-Allow-Origin',
-    'https://foodexplorer1000.netlify.app/',
+    'https://foodexplorer1000.netlify.app',
   )
   res.setHeader('Access-Control-Allow-Credentials', 'true')
   next()
